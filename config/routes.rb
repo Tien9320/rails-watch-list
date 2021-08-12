c5e04a46e09afc57982ed17ccbc4b39a38bda1e7
Tien Nguyen <thuytienng090300@gmail.com>
2021-08-12 14:25:42 -0400

    Passed 41 rake examples

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
+  resources :bookmarks, only: %i[new create destroy]
+  resources :lists, only: %i[index show new create destroy] do
+    resources :bookmarks, only: %i[new create]
+  end
 end
